98c4bcfd36a32804f13dee69ec177ba4b2a8a2c8
djmary-k <kondratyukmv@gmail.com>
2023-09-14 16:23:13 +0400

    created files init and setup
    - created init file
    -created setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='App_assistant',
+    version='1.0.0',
+    description='Console bot that gives opportunity to save contacts, creat and save notes and sort files.',
+    url='https://github.com/djmary-k/App_assistant.git',
+    author='Maryna Kondratiuk, Tetiana Shevchenko, Anastasiia Kysliak, Volodymyr Mazurets, Oleksandr Semochkin',
+    author_email='kondratyukmv@gmail.com',
+    license='MIT',
+    packages=find_namespace_packages(),
+    install_requires=['prettytable'],
+    entry_points={'console_scripts': ['yfa=main:run']}
+)
